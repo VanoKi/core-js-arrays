a750b0b008b03d20146f7a4f4b33a5660b51e8b5
VanoKi <kirsvano@gmail.com>
2024-01-04 13:31:47 +0300

    propagateItem solved

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -451,6 +451,7 @@ function getMaxItems(arr, n) {
     .reverse()
     .slice(0, n);
 }
+
 /**
  * Finds and returns an array containing only the common elements found in two arrays.
  *
@@ -496,8 +497,8 @@ function findLongestIncreasingSubsequence(/* nums */) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.map((e, i) => Array(i + 1).fill(e)).flat(Infinity);
 }
 
 /**
